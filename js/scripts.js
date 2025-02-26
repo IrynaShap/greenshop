867e3fa0830785b0b87f1ae10ec387db80c957fa
IrynaShap <angeliawia@gmail.com>
2025-02-26 19:18:44 +0200

    "Add OwlCarousel v2.3.4 library to project"

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$('.first-carousel').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 6500,
+    autoplaySpeed: 1000,
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:2
+        }
+    }
+
+});
+$('.second-carousel').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 6500,
+    autoplaySpeed: 1000,
+    loop: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        1100: {
+            items: 4
+        }
+    }
+});
